08eef5f884a123605fd50e19c1610dd53eafe5a6
tiasz <iskiizeh3@gmail.com>
2025-07-26 10:06:03 -0300

    setting the window

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+
+from constants import *
+
+
+def main():
+    """
+    The function that runs the game or better, our project
+    """
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
